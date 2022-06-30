0d92e6e2919ea8218f834701cd71c70b847c6874
razakadegoke <razakadegoke@outlook.fr>
2022-06-30 10:48:24 -0400

    pull from stage2

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/quote'
-  get 'home/commercial'
-  get 'home/residential'
-end
-=======
 Rails.application.routes.draw do
   resources :employees
   devise_for :users
   root 'home#index'
   get 'home/backOffice'
+  get 'home/quote'
+  get 'home/commercial'
+  get 'home/residential'
 end
->>>>>>> 117f77d02f446c68f8c77b604a577f64cf8d8491
