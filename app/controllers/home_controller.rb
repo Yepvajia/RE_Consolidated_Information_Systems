a5c7f95a6c457a8b7d81b39a37809031825395e3
Avinashbbb <avinash.bbb@gmail.com>
2022-06-30 18:15:20 -0400

    i have managed to make most of the admin sections work. this includes only admins can ender the page. need to enter the quote data in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    if current_user
-      redirect_to rails_admin_path
-    end
+    # if current_user
+    #   redirect_to rails_admin_path
+    # end
   end
   
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,12 +1,15 @@
 RailsAdmin.config do |config|
-
+config.authorize_with do
+  redirect_to main_app.root_path unless warden.user.admin == 1
+  
+end
   ### Popular gems integration
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  #== Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
