cc398ee1cc1781d9492271bd6c391be180a6dd22
razakadegoke <razakadegoke@outlook.fr>
2022-07-01 15:15:10 -0400

    commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
+//= require bootstrap.min
+//= require jquery-3.3.1.min
+//= require owl.carousel
+
