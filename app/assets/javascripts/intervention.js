706e1f03351b7ee1fc778fb8cc7bd41a3ec2ebfc
Yepvajia <yepper.dev@gmail.com>
2022-07-27 21:24:24 -0400

    seed is even better, more form/js stuff

--- app/assets/javascripts/intervention.js
+++ app/assets/javascripts/intervention.js
@@ -1,9 +1,13 @@
 
 $("#hello").click(function(ev){
   ev.preventDefault();
-  var juice
-  fetch("https://cors-proxy-yepper.herokuapp.com/https://roc-yepper.herokuapp.com/api/elevator/"+23)
+  // var juice
+  fetch("https://cors-proxy-yepper.herokuapp.com/https://roc-yepper.herokuapp.com/api/elevator/"+21)
     .then((response) => response.json())
     .then((data) => console.log(data));
   // console.log(juice);
 });
+
+$(".db-id").change(function() {
+  console.log("JUICE");
+})
